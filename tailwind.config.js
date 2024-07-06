b4fe721225e055491d473b25a803ca3594004549
zoeshoryX4 <bird.villedax@gmail.com>
2024-07-05 22:59:25 -0500

    cambio de estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,25 @@ module.exports = {
   theme: {
     extend: {
 
+
+
       fontFamily: {
         "haken": ['"Hanken Grotesk"', 'sans-serif']
       },
 
       colors: {
-        "light-slate-blue": "hsl(252, 100%, 67%)",
-        "light-royal-blue": "hsl(241, 81%, 54%)",
-        "violet-blue": "hsla(256, 72%, 46%, 1)",
-        "persian-blue": "hsla(241, 72%, 46%, 0)",
+        "light-slate-blue": "#0f766e",
+        "light-royal-blue": "#115e59",
+        "violet-blue": "#115e59",
+        "persian-blue": "#134e4a",
         "light-red": "hsl(0, 100%, 67%)",
         "orangey-yellow": "hsl(39, 100%, 56%)",
         "green-teal": "hsl(166, 100%, 37%)",
         "cobalt-blue": "hsl(234, 85%, 45%)",
         "pale-blue": "hsl(221, 100%, 96%)",
-        "light-lavender": "hsl(241, 100%, 89%)",
-        "dark-gray-blue": "hsl(224, 30%, 27%)"
+        "light-lavender": "#d1fae5",
+        "dark-gray-blue": "hsl(224, 30%, 27%)",
+
       }
 
     },
